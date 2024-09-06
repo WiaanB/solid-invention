76f770564fddeb93f15597e0268691fb0eeb7e28
WiaanB <wiaan.dev@gmail.com>
2024-09-06 18:42:00 +0200

    added a serve command with a basic web server using gin

---
+++ cmd/serve.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"gotcha/server"
+)
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+}
+
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Serves the Gotcha Web Server",
+	Run: func(cmd *cobra.Command, args []string) {
+		server.Serve()
+	},
+}
